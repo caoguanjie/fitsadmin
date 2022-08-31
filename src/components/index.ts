29f2c46f4a2508e36959bab1c05eefc579b112b2
caoguanjie <374348532@qq.com>
2022-08-31 20:45:31 +0800

    更新组件库文件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,9 @@
 import ButtonTest from './Form/ButtonTest.vue'
-import TreeSelect from './Form/TreeSelect.vue'
 import type { App } from 'vue'
 
 const FitAdminUI = {
     install: (app: App<Element>) => {
         app.component('FitsButtonTest', ButtonTest);
-        app.component('FitsTreeSelect', TreeSelect);
     }
 }
 
