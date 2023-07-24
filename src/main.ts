682394bbdcd7e48e7d6ae69b0c0f9ed3420ffc99
caoguanjie <374348532@qq.com>
2023-07-24 17:02:50 +0800

    formcreate的样式冲突，导致表达弹框错乱，调整位置

--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,17 @@ import * as directive from '@/directive';
 import { setupStore } from '@/store';
 // default-passive-events会影响工作流范例的使用
 import 'default-passive-events'
-import setupOtherImports from './other-imports';
-import formCreate from '@form-create/element-ui';
 // 目前不支持自动按需加载, 需要手动导入一下 auto-import.js 文件, 详情：https://github.com/xaboy/form-create/issues/511
 import install from '@form-create/element-ui/auto-import'
+import formCreate from '@form-create/element-ui';
+import setupOtherImports from './other-imports';
 // 自定义样式
 import '@/styles/index.scss';
 const app = createApp(App);
-formCreate.use(install)
+
 Object.keys(directive).forEach((key) => {
   app.directive(key, (directive as { [key: string]: Directive })[key]);
 });
-
 
 
 // 挂载路由
@@ -30,6 +29,8 @@ setupRouter(app);
 setupStore(app);
 // 全局注册更多第三方的组件库、插件等内容
 setupOtherImports(app)
+
+formCreate.use(install)
 
 // 注册全局组件
 app.use(formCreate).mount('#app');
