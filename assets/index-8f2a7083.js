9344397b22326b6255dc4b68ef17b8a3fabcd04e
caoguanjie <374348532@qq.com>
2023-08-03 07:45:58 +0000

    Deploying to frame from @ caoguanjie/fitsadmin@7d5100fa222faa94dce50c4884f7bca43b06968c 🚀

--- assets/index-8f2a7083.js
+++
@@ -1,1 +0,0 @@
-import{u as a,a as e}from"./index-68eff4a3.js";import{E as s,l as r,m as t}from"./vxetable-f32c91e3.js";import"./echarts-1215139a.js";const p=s({__name:"index",setup(s){const p=a(),m=e(),{params:o,query:i}=p,{path:n}=o;return m.replace({path:"/"+n,query:i}),(a,e)=>(r(),t("div"))}});export{p as default};
