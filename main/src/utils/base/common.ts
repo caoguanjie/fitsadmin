33bbd7dabedb27dfb7f7fa4dfe88ad43cee507f0
caoguanjie <374348532@qq.com>
2023-09-26 15:40:52 +0800

    计算主要内容区域的主要高度

--- main/src/utils/base/common.ts
+++ main/src/utils/base/common.ts
@@ -23,3 +23,29 @@ export const useCountdown = () => {
 
   return { start, time }
 }
+
+/**
+ * 计算主要内容区域的主要高度
+ * 会根据拉伸浏览器，及时调整可滑动的高度值
+ * @param reduceNumber 入参是需要减去的数值，例如head的高度，tab的高度，footer的高度。
+ * @returns {string} 单位px
+ */
+export const useComputedAutoHeight = (reduceNumber = 0) => {
+  const bodyHeight = ref(document.body.clientHeight)
+  const scrollerHeight = computed(() => {
+    return (bodyHeight.value - reduceNumber) + 'px'
+  })
+  // 监听浏览器的变化，及时更新scorll组件的高度
+  const resize = useResizeObserver(document.body, (entries) => {
+    const entry = entries[0]
+    const { height } = entry.contentRect
+    bodyHeight.value = height
+  })
+
+  onUnmounted(() => {
+    // dom节点销毁时，要及时销毁浏览器的监听事件，避免内存泄露
+    resize.stop();
+  })
+
+  return { scrollerHeight }
+}
