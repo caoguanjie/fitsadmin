c34692f835eef66ecf82a505307a5485da3d966f
caoguanjie <374348532@qq.com>
2023-10-17 10:01:36 +0800

    删除多余的代码

--- main/src/App.vue
+++ main/src/App.vue
@@ -11,21 +11,16 @@ import zhCn from 'element-plus/lib/locale/lang/zh-cn';
 
 import useStore from '@/store';
 import { useRoute } from 'vue-router';
-import { useMicroFrontendsStore } from './store/base/micro-frontends-test';
+
 
 // 导入 Element Plus 语言包
 
 
 const { app } = useStore();
 
-const { subURL } = useMicroFrontendsStore()
+
 const size: any = computed(() => app.size);
 
 const locale = ref();
-setTimeout(() => {
-  // subURL.name = 3
-  console.log(subURL)
-  app.language = 'zh'
-}, 3000)
 
 </script>
