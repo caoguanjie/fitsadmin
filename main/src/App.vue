2bc7ab15c2a642a915860b1aa1e383fb02733d62
caoguanjie <374348532@qq.com>
2023-10-25 16:39:42 +0800

    删除vue的补丁，升级vue@3.3.7版本，升级element-plus@2.4.1

--- main/src/App.vue
+++ main/src/App.vue
@@ -7,8 +7,7 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import { ElConfigProvider } from 'element-plus';
-import zhCn from 'element-plus/lib/locale/lang/zh-cn';
-
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
 
 import useStore from '@/store';
 
